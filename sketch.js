51e220e1a3d420127363e309d929c759ee7c960b
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-25 19:21:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,5 +3,9 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(100,100,200);
+  //Followed some hunches and figured out the syntax 
+  for(var i = 0; i < 10; i++) {
+    rect(20 * i,20 * i,30 * i,i * 30,i);
+  }
 }
